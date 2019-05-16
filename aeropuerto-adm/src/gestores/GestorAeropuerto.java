cd13c8ab5190213e4f51758cbf9cf088484a94ed
matiasdlf <matiasdlf90@gmail.com>
2019-05-15 23:59:34 -0300

    Se corrigieron errores en GestorAeropuerto y en testGestorAeropuerto

--- aeropuerto-adm/src/gestores/GestorAeropuerto.java
+++ aeropuerto-adm/src/gestores/GestorAeropuerto.java
@@ -1,5 +1,39 @@
 package gestores;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dominio.Producto;
+import paquete00.Aeropuerto;
+
 public class GestorAeropuerto {
+	List<Aeropuerto> listaAeropuerto = new ArrayList<Aeropuerto>();
+	static private GestorAeropuerto ga;
+	
+	private GestorAeropuerto(ArrayList<Aeropuerto> arrayList) {
+		listaAeropuerto= arrayList;
+	}
+	
+	static public GestorAeropuerto getInstance() {
+		if (ga == null) {
+			ga = new GestorAeropuerto(new ArrayList<Aeropuerto>());
+		}
+		return ga;
+	}
+
+	public boolean createAeropuerto(Aeropuerto p) {
+		return listaAeropuerto.add(p);
+	}
+
+	public boolean traerUno(Integer i) {
+		int contador = 0;
+		for (Aeropuerto a : listaAeropuerto) {
+			if (a.getCodigo().equals(i)) {
+				break;
+			}
+			return false;
+		}	
+		return true;
+	}
 	
 }
--- aeropuerto-adm/src/testGestores/TestGestorCompletoAeropuerto.java
+++ aeropuerto-adm/src/testGestores/TestGestorCompletoAeropuerto.java
@@ -10,15 +10,23 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import gestores.GestorAeropuerto;
+import paquete00.Aeropuerto;
+import paquete00.Ciudad;
 
 class TestGestorCompletoAeropuerto {
 
 	@Test
 	void testGestorCrearAeropuerto() {
-		GestorAeropuerto ga1 = GestorAeropuerto.getInstancia();
-		assertEquals(true, ga1.createAeropuerto(001, "Aeropuerto Ezeiza", "Buenos Aires", "EZ-01"));
-		assertEquals(true, ga1.createAeropuerto(002, "Aeropuerto Anguinan", "Anguinan", "ANG-02"));
-		assertEquals(true, ga1.createAeropuerto(003, "Aeropuerto Giovanni Nicelli", "Venecia", "VN-03"));
+		GestorAeropuerto ga1 = GestorAeropuerto.getInstance();
+		Ciudad c1 = new Ciudad(1, "Buenos Aires", "BSAS");
+		Aeropuerto a1= new Aeropuerto(001, "Aeropuerto Ezeiza", c1, "EZ-01");
+		assertEquals(true, ga1.createAeropuerto(a1));
+		Ciudad c2 = new Ciudad(2, "Anguinan", "ANG");
+		Aeropuerto a2= new Aeropuerto(002, "Aeropuerto Anguinan", c2, "ANG-02");
+		assertEquals(true, ga1.createAeropuerto(a2));
+		Ciudad c3= new Ciudad(3, "Venecia", "VEN");
+		Aeropuerto a3= new Aeropuerto(003, "Aeropuerto Ezeiza", c3, "EZ-01");
+		assertEquals(true, ga1.createAeropuerto(a3));
 			
 	}
 	
